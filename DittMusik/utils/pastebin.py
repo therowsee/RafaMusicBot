932e66c2edb2007a825798a54c8e9f27438fd0e7
therowsee <108076029+therowsee@users.noreply.github.com>
2022-08-18 15:03:36 +0700

    Update pastebin.py

--- DittMusik/utils/pastebin.py
+++ DittMusik/utils/pastebin.py
@@ -1,9 +1,9 @@
 #
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+# Copyright (C) 2021-2022 by therowsee@Github, < https://github.com/therowsee >.
 #
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# This file is part of < https://github.com/therowsee/DitMusik > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+# Please see < https://github.com/therowsee/DitMusik/blob/master/LICENSE >
 #
 # All rights reserved.
 
@@ -22,7 +22,7 @@ async def post(url: str, *args, **kwargs):
         return data
 
 
-async def Yukkibin(text):
+async def therowseebin(text):
     resp = await post(f"{BASE}api/v2/paste", data=text)
     if not resp["success"]:
         return
