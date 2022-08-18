5c4d08d3428e9a0f555c25bac1356968ef0b4661
therowsee <108076029+therowsee@users.noreply.github.com>
2022-08-19 01:30:12 +0700

    Update playlist.py

--- DittMusik/plugins/play/playlist.py
+++ DittMusik/plugins/play/playlist.py
@@ -17,16 +17,16 @@ from pyrogram.types import (InlineKeyboardButton,
 
 from config import BANNED_USERS, SERVER_PLAYLIST_LIMIT
 from strings import get_command
-from DitxynzBot import Carbon, YouTube, app
-from DitxynzBot.utils.database import (delete_playlist, get_playlist,
+from DitMusik import Carbon, YouTube, app
+from DitMusik.utils.database import (delete_playlist, get_playlist,
                                        get_playlist_names,
                                        save_playlist)
-from DitxynzBot.utils.decorators.language import language, languageCB
-from DitxynzBot.utils.inline.playlist import (botplaylist_markup,
+from DitMusik.utils.decorators.language import language, languageCB
+from DitMusik.utils.inline.playlist import (botplaylist_markup,
                                               get_playlist_markup,
                                               warning_markup)
-from DitxynzBot.utils.pastebin import therowseebin
-from DitxynzBot.utils.stream.stream import stream
+from DitMusik.utils.pastebin import DitMusikbin
+from DitMusik.utils.stream.stream import stream
 
 # Command
 PLAYLIST_COMMAND = get_command("PLAYLIST_COMMAND")
@@ -55,7 +55,7 @@ async def check_playlist(client, message: Message, _):
         count += 1
         msg += f"\n\n{count}- {title[:70]}\n"
         msg += _["playlist_5"].format(duration)
-    link = await therowseebin(msg)
+    link = await DitMusikbin(msg)
     lines = msg.count("\n")
     if lines >= 17:
         car = os.linesep.join(msg.split(os.linesep)[:17])
