874cb25ffcc288349eb484d443c026abccca0ed8
therowsee <108076029+therowsee@users.noreply.github.com>
2022-08-19 00:47:28 +0700

    Update stop.py

--- DittMusik/plugins/admins/stop.py
+++ DittMusik/plugins/admins/stop.py
@@ -31,7 +31,7 @@ STOP_COMMAND = get_command("STOP_COMMAND")
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return await message.reply_text(_["general_2"])
-    await Yukki.stop_stream(chat_id)
+    await DitMusik.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
         _["admin_9"].format(message.from_user.mention)
