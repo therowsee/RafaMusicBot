c97bea06c91a28540b14d96a8d8a54afe25fb824
therowsee <108076029+therowsee@users.noreply.github.com>
2022-08-19 00:44:34 +0700

    Update skip.py

--- DittMusik/plugins/admins/skip.py
+++ DittMusik/plugins/admins/skip.py
@@ -120,7 +120,7 @@ async def skip(cli, message: Message, _, chat_id):
                 _["admin_11"].format(title)
             )
         try:
-            await therowsee.skip_stream(chat_id, link, video=status)
+            await DitMusik.skip_stream(chat_id, link, video=status)
         except Exception:
             return await message.reply_text(_["call_9"])
         button = telegram_markup(_)
@@ -149,7 +149,7 @@ async def skip(cli, message: Message, _, chat_id):
         except:
             return await mystic.edit_text(_["call_9"])
         try:
-            await therowsee.skip_stream(chat_id, file_path, video=status)
+            await DitMusik.skip_stream(chat_id, file_path, video=status)
         except Exception:
             return await mystic.edit_text(_["call_9"])
         button = stream_markup(_, videoid)
@@ -167,7 +167,7 @@ async def skip(cli, message: Message, _, chat_id):
         await mystic.delete()
     elif "index_" in queued:
         try:
-            await therowsee.skip_stream(chat_id, videoid, video=status)
+            await DitMusik.skip_stream(chat_id, videoid, video=status)
         except Exception:
             return await message.reply_text(_["call_9"])
         button = telegram_markup(_)
@@ -180,7 +180,7 @@ async def skip(cli, message: Message, _, chat_id):
         db[chat_id][0]["markup"] = "tg"
     else:
         try:
-            await therowsee.skip_stream(chat_id, queued, video=status)
+            await DitMusik.skip_stream(chat_id, queued, video=status)
         except Exception:
             return await message.reply_text(_["call_9"])
         if videoid == "telegram":
