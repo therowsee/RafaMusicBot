12f2e25d7a960e684eadc9f316377e7a060efee3
therowsee <108076029+therowsee@users.noreply.github.com>
2022-08-18 14:22:01 +0700

    Update languages.py

--- DittMusik/plugins/tools/languages.py
+++ DittMusik/plugins/tools/languages.py
@@ -1,9 +1,9 @@
 #
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+# Copyright (C) 2021-2022 by therowsee@Github, < https://github.com/therowsee >.
 #
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# This file is part of < https://github.com/therowsee/DitMusik > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+# Please see < https://github.com/therowsee/DitMusik/blob/master/LICENSE >
 #
 # All rights reserved.
 
@@ -13,9 +13,9 @@ from pyrogram.types import InlineKeyboardButton, Message
 
 from config import BANNED_USERS
 from strings import get_command, get_string
-from YukkiMusic import app
-from YukkiMusic.utils.database import get_lang, set_lang
-from YukkiMusic.utils.decorators import (ActualAdminCB, language,
+from DitxynzBot import app
+from DitxynzBot.utils.database import get_lang, set_lang
+from DitxynzBot.utils.decorators import (ActualAdminCB, language,
                                          languageCB)
 
 # Languages Available
