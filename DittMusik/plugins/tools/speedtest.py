52db732ea910296cfe981f335525f656a81c0574
therowsee <108076029+therowsee@users.noreply.github.com>
2022-08-18 14:33:41 +0700

    Update speedtest.py

--- DittMusik/plugins/tools/speedtest.py
+++ DittMusik/plugins/tools/speedtest.py
@@ -1,9 +1,9 @@
 #
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+# Copyright (C) 2021-2022 by therowsee@Github, < https://github.com/therowsee >.
 #
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# This file is part of < https://github.com/therowsee/DitMusik > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+# Please see < https://github.com/therowsee/DitMusik/blob/master/LICENSE >
 #
 # All rights reserved.
 
@@ -15,8 +15,8 @@ import wget
 from pyrogram import filters
 
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
+from DitxynzBot import app
+from DitxynzBot.misc import SUDOERS
 
 # Commands
 SPEEDTEST_COMMAND = get_command("SPEEDTEST_COMMAND")
