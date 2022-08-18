c1fd7aa55712b588e7458cbf7b265983540a01a3
therowsee <108076029+therowsee@users.noreply.github.com>
2022-08-18 14:41:59 +0700

    Update language.py

--- DittMusik/utils/decorators/language.py
+++ DittMusik/utils/decorators/language.py
@@ -1,16 +1,16 @@
 #
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+# Copyright (C) 2021-2022 by therowsee@Github, < https://github.com/therowsee >.
 #
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# This file is part of < https://github.com/therowsee/DitMusik > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+# Please see < https://github.com/therowsee/DitMusik/blob/master/LICENSE >
 #
 # All rights reserved.
 
 
 from strings import get_string
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database import (get_lang, is_commanddelete_on,
+from DitxynzBot.misc import SUDOERS
+from DitxynzBot.utils.database import (get_lang, is_commanddelete_on,
                                        is_maintenance)
 
 
