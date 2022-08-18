42fac8bc62c43b18642db5b32e70b032922412d4
therowsee <108076029+therowsee@users.noreply.github.com>
2022-08-18 12:10:37 +0700

    Update start.py

--- DittMusik/plugins/bot/start.py
+++ DittMusik/plugins/bot/start.py
@@ -9,18 +9,18 @@ import config
 from config import BANNED_USERS
 from config.config import OWNER_ID
 from strings import get_command, get_string
-from YukkiMusic import Telegram, YouTube, app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.plugins.play.playlist import del_plist_msg
-from YukkiMusic.plugins.sudo.sudoers import sudoers_list
-from YukkiMusic.utils.database import (add_served_chat,
+from DitxynzBot import Telegram, YouTube, app
+from DitxynzBot.misc import SUDOERS
+from DitxynzBot.plugins.play.playlist import del_plist_msg
+from DitxynzBot.plugins.sudo.sudoers import sudoers_list
+from DitxynzBot.utils.database import (add_served_chat,
                                        add_served_user,
                                        blacklisted_chats,
                                        get_assistant, get_lang,
                                        get_userss, is_on_off,
                                        is_served_private_chat)
-from YukkiMusic.utils.decorators.language import LanguageStart
-from YukkiMusic.utils.inline import (help_pannel, private_panel,
+from DitxynzBot.utils.decorators.language import LanguageStart
+from DitxynzBot.utils.inline import (help_pannel, private_panel,
                                      start_pannel)
 
 loop = asyncio.get_running_loop()
