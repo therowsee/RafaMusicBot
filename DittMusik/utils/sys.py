8c58b26d5433313f63fdeb6b101e77e38aa72cc1
therowsee <108076029+therowsee@users.noreply.github.com>
2022-08-18 15:04:09 +0700

    Update sys.py

--- DittMusik/utils/sys.py
+++ DittMusik/utils/sys.py
@@ -1,9 +1,9 @@
 #
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+# Copyright (C) 2021-2022 by therowsee@Github, < https://github.com/therowsee >.
 #
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# This file is part of < https://github.com/therowsee/DitMusik > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+# Please see < https://github.com/therowsee/DitMusik/blob/master/LICENSE >
 #
 # All rights reserved.
 
@@ -11,7 +11,7 @@ import time
 
 import psutil
 
-from YukkiMusic.misc import _boot_
+from DitxynzBot.misc import _boot_
 
 from .formatters import get_readable_time
 
