0910474df6cc678d9b3a4e855391caa14f863646
therowsee <108076029+therowsee@users.noreply.github.com>
2022-08-18 16:37:02 +0700

    Update bot.py

--- DittMusik/core/bot.py
+++ DittMusik/core/bot.py
@@ -16,7 +16,7 @@ import config
 from ..logging import LOGGER
 
 
-class DitxynzBot(Client):
+class DitxyzBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot")
         super().__init__(
