9b05494bf2c05472445f0ddc6c81f26514a56e62
therowsee <108076029+therowsee@users.noreply.github.com>
2022-08-20 00:38:59 +0700

    Update help.py

--- DittMusik/utils/inline/help.py
+++ DittMusik/utils/inline/help.py
@@ -11,7 +11,7 @@ from typing import Union
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from DitxynzBot import app
+from DitMusik  import app
 
 
 def help_pannel(_, START: Union[bool, int] = None):
