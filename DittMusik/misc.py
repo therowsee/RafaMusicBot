a4eac6da5bdb1e29b9215cf9c56dd664cdc50299
therowsee <108076029+therowsee@users.noreply.github.com>
2022-08-20 01:02:14 +0700

    Update misc.py

--- DittMusik/misc.py
+++ DittMusik/misc.py
@@ -14,7 +14,7 @@ import heroku3
 from pyrogram import filters
 
 import config
-from DitxynzBot.core.mongo import pymongodb
+from DitMusik.core.mongo import pymongodb
 
 from .logging import LOGGER
 
