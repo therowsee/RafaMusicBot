a3b3e7b9b6bad2f76e1400c5c94652a643fb83fd
therowsee <108076029+therowsee@users.noreply.github.com>
2022-08-20 00:40:38 +0700

    Update play.py

--- DittMusik/utils/inline/play.py
+++ DittMusik/utils/inline/play.py
@@ -89,11 +89,11 @@ def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
         [
             InlineKeyboardButton(
                 text=_["P_B_1"],
-                callback_data=f"YukkiPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
+                callback_data=f"DitMusikPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
             ),
             InlineKeyboardButton(
                 text=_["P_B_2"],
-                callback_data=f"YukkiPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
+                callback_data=f"DitMusikPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
             ),
         ],
         [
