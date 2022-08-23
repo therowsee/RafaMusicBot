cac4661e617ae663d5c53c49669a5b43322de213
therowsee <108076029+therowsee@users.noreply.github.com>
2022-08-24 06:21:17 +0700

    Update songs.py

--- DittMusik/plugins/tools/songs.py
+++ DittMusik/plugins/tools/songs.py
@@ -20,10 +20,10 @@ from pyrogram.types import (InlineKeyboardButton,
 from config import (BANNED_USERS, SONG_DOWNLOAD_DURATION,
                     SONG_DOWNLOAD_DURATION_LIMIT)
 from strings import get_command
-from DitxynzBot import YouTube, app
-from DitxynzBot.utils.decorators.language import language, languageCB
-from DitxynzBot.utils.formatters import convert_bytes
-from DitxynzBot.utils.inline.song import song_markup
+from therowsee import YouTube, app
+from therowsee.utils.decorators.language import language, languageCB
+from therowsee.utils.formatters import convert_bytes
+from therowsee.utils.inline.song import song_markup
 
 # Command
 SONG_COMMAND = get_command("SONG_COMMAND")
