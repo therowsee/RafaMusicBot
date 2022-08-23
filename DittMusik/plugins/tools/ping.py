8f849a0893b6bb874bf1c00ed0252135c3cd646d
therowsee <108076029+therowsee@users.noreply.github.com>
2022-08-24 06:17:40 +0700

    Update ping.py

--- DittMusik/plugins/tools/ping.py
+++ DittMusik/plugins/tools/ping.py
@@ -14,10 +14,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS, MUSIC_BOT_NAME, PING_IMG_URL
 from strings import get_command
-from DitMusik import app
-from DitMusik.core.call import DitMusik
-from DitMusik.utils import bot_sys_stats
-from DitMusik.utils.decorators.language import language
+from therowsee import app
+from therowsee.core.call import DitMusik
+from therowsee.utils import bot_sys_stats
+from therowsee.utils.decorators.language import language
 
 ### Commands
 PING_COMMAND = get_command("PING_COMMAND")
