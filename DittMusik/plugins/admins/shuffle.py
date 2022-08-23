63da56bf16763b18c1091310f05ba7d88f3b3cdc
therowsee <108076029+therowsee@users.noreply.github.com>
2022-08-24 05:46:57 +0700

    Update shuffle.py

--- DittMusik/plugins/admins/shuffle.py
+++ DittMusik/plugins/admins/shuffle.py
@@ -14,9 +14,9 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from DitMusik import app
-from DitMusik.misc import db
-from DitMusik.utils.decorators import AdminRightsCheck
+from therowsee import app
+from therowsee.misc import db
+from therowsee.utils.decorators import AdminRightsCheck
 
 # Commands
 SHUFFLE_COMMAND = get_command("SHUFFLE_COMMAND")
