58f584673c53c9c040c885988287ec458de67d9b
therowsee <108076029+therowsee@users.noreply.github.com>
2022-08-24 06:26:25 +0700

    Update mongodatabase.py

--- DittMusik/utils/database/mongodatabase.py
+++ DittMusik/utils/database/mongodatabase.py
@@ -9,7 +9,7 @@
 
 from typing import Dict, List, Union
 
-from DitMusik.core.mongo import mongodb
+from therowsee.core.mongo import mongodb
 
 queriesdb = mongodb.queries
 userdb = mongodb.userstats
