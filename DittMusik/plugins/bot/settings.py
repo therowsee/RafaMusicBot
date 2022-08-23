f2efcdd268bb0acfa5baf96be0dfd09e7f3963e2
therowsee <108076029+therowsee@users.noreply.github.com>
2022-08-24 05:52:50 +0700

    Update settings.py

--- DittMusik/plugins/bot/settings.py
+++ DittMusik/plugins/bot/settings.py
@@ -15,8 +15,8 @@ from pyrogram.types import (CallbackQuery, InlineKeyboardButton,
 from config import (BANNED_USERS, CLEANMODE_DELETE_MINS,
                     MUSIC_BOT_NAME, OWNER_ID)
 from strings import get_command
-from DitMusik import app
-from DitMusik.utils.database import (add_nonadmin_chat,
+from therowsee import app
+from therowsee.utils.database import (add_nonadmin_chat,
                                        cleanmode_off, cleanmode_on,
                                        commanddelete_off,
                                        commanddelete_on,
@@ -33,13 +33,13 @@ from DitMusik.utils.database import (add_nonadmin_chat,
                                        save_video_bitrate,
                                        set_playmode, set_playtype,
                                        suggestion_off, suggestion_on)
-from DitMusik.utils.decorators.admins import ActualAdminCB
-from DitMusik.utils.decorators.language import language, languageCB
-from DitMusik.utils.inline.settings import (
+from therowsee.utils.decorators.admins import ActualAdminCB
+from therowsee.utils.decorators.language import language, languageCB
+from therowsee.utils.inline.settings import (
     audio_quality_markup, auth_users_markup,
     cleanmode_settings_markup, playmode_users_markup, setting_markup,
     video_quality_markup)
-from DitMusik.utils.inline.start import private_panel
+from therowsee.utils.inline.start import private_panel
 
 ### Command
 SETTINGS_COMMAND = get_command("SETTINGS_COMMAND")
