5d9e1db99d44f1a0305d89472aed3d1bb26825e0
therowsee <108076029+therowsee@users.noreply.github.com>
2022-08-24 05:44:01 +0700

    Update loop.py

--- DittMusik/plugins/admins/loop.py
+++ DittMusik/plugins/admins/loop.py
@@ -12,10 +12,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from DitMusik import app
-from DitMusik.utils.database.memorydatabase import (get_loop,
+from therowsee import app
+from therowsee.utils.database.memorydatabase import (get_loop,
                                                       set_loop)
-from DitMusik.utils.decorators import AdminRightsCheck
+from therowsee.utils.decorators import AdminRightsCheck
 
 # Commands
 LOOP_COMMAND = get_command("LOOP_COMMAND")
