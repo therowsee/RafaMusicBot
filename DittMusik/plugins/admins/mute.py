4ce3b0d4f4ac1ff33056914d064ccd7599ccf47c
therowsee <108076029+therowsee@users.noreply.github.com>
2022-08-25 05:12:47 +0700

    Update mute.py

--- DittMusik/plugins/admins/mute.py
+++ DittMusik/plugins/admins/mute.py
@@ -13,7 +13,7 @@ from pyrogram.types import Message
 from config import BANNED_USERS
 from strings import get_command
 from DittMusik import app
-from DittMusik.core.call import DitMusik
+from DittMusik.core.call import Ditt
 from DittMusik.utils.database import is_muted, mute_on
 from DittMusik.utils.decorators import AdminRightsCheck
 
