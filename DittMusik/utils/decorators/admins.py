17a39981054195bb22f1a7a514a85b3926c284c2
therowsee <108076029+therowsee@users.noreply.github.com>
2022-08-25 01:55:23 +0700

    Update admins.py

--- DittMusik/utils/decorators/admins.py
+++ DittMusik/utils/decorators/admins.py
@@ -11,9 +11,9 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from config import adminlist
 from strings import get_string
-from therowsee import app
-from therowsee.misc import SUDOERS
-from therowsee.utils.database import (get_authuser_names, get_cmode,
+from DittMusik import app
+from DittMusik.misc import SUDOERS
+from DittMusik.utils.database import (get_authuser_names, get_cmode,
                                        get_lang, is_active_chat,
                                        is_commanddelete_on,
                                        is_maintenance,
