66b8c4ff92e123ffa82cb0441b610a2cd07bb5c2
therowsee <108076029+therowsee@users.noreply.github.com>
2022-08-25 01:43:49 +0700

    Update sudoers.py

--- DittMusik/plugins/sudo/sudoers.py
+++ DittMusik/plugins/sudo/sudoers.py
@@ -12,10 +12,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS, MONGO_DB_URI, OWNER_ID
 from strings import get_command
-from therowsee import app
-from therowsee.misc import SUDOERS
-from therowsee.utils.database import add_sudo, remove_sudo
-from therowsee.utils.decorators.language import language
+from DittMusik import app
+from DittMusik.misc import SUDOERS
+from DittMusik.utils.database import add_sudo, remove_sudo
+from DittMusik.utils.decorators.language import language
 
 # Command
 ADDSUDO_COMMAND = get_command("ADDSUDO_COMMAND")
